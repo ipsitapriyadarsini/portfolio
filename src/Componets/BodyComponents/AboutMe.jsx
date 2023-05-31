8b1e3235e009a0612ae46a7de1199ea831604187
ipsitapriyadarsini <41966804+ipsitapriyadarsini@users.noreply.github.com>
2023-05-31 13:03:24 +0530

    Added about me and removed the color full transparency screen

--- src/Componets/BodyComponents/AboutMe.jsx
+++ src/Componets/BodyComponents/AboutMe.jsx
@@ -7,30 +7,17 @@ const AboutMe = () => {
     <>
       <div className="aboutme_container">
         <div className="content">
-          <h2>About Me</h2>
+          <h2>ABOUT ME</h2>
+          <br />
+          <h3>A dedicated Front-end Developer based in Puri, Odisha, India</h3>
           <br />
           <p>
-
-            Hi there! I'm IPSITA PRIYADARSINI, a passionate React front-end developer with 3 years of experience in building modern
-            and user-friendly web applications. I love turning ideas into reality and crafting intuitive interfaces that make a positive impact.</p>
+            As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Typescript, Bootstrap, Tailwind,  SCSS and Material-UI.
+            I excel in designing and maintaining responsive
+            websites that offer a smooth user experience.</p>
+          My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques.
+          I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.
           <br />
-          <p>My journey in web development began from the year.
-            Since then, I have immersed myself in the world of React, continuously honing my skills to stay up-to-date with the latest technologies and industry best practices.</p>
-          <br />
-          <p>Proficient in JavaScript, HTML, CSS, and of course, React, I have successfully delivered [mention number] projects, collaborating with diverse teams and clients. My expertise lies in crafting responsive layouts, optimizing performance, and implementing scalable solutions to meet specific project requirements.</p>
-          <br />
-          <p>I have a strong understanding of front-end development principles, including component-based architecture, state management (such as Redux or Context API), and modern tooling (Webpack, Babel, etc.). I strive to write clean, modular, and maintainable code, following industry standards and accessibility guidelines.</p>
-          <br />
-          <p>In addition to technical skills, I possess excellent problem-solving abilities and a keen eye for detail. I enjoy working collaboratively, actively participating in brainstorming sessions, and offering innovative solutions to design and functionality challenges.</p>
-          <br />
-          <p>Continuous learning is essential to me, and I'm always exploring new technologies and frameworks to expand my knowledge. I value open communication, feedback, and staying adaptable in a fast-paced development environment.</p>
-          <br />
-          <p>When I'm not coding, you can find me [mention any relevant hobbies, interests, or community involvement]. I believe in the power of technology to shape the world positively, and I'm excited about contributing to projects that have a meaningful impact.</p>
-          <br />
-          <p>Thank you for taking the time to visit my portfolio. I'm eager to connect, collaborate, and bring your ideas to life. Let's create something remarkable together!</p>
-
-          <br />
-
           <p> Feel free to personalize the template based on your own experiences, skills, and personality. Remember to keep it concise, engaging, and focused on highlighting your strengths as a React front-end developer. Good luck with your portfolio!</p>
         </div>
         <div className="ipsita">
--- src/Componets/BodyComponents/HeroSection.jsx
+++ src/Componets/BodyComponents/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "../../assets/images/desktop.jpg";
+import { Bounce, Slide } from "react-awesome-reveal";
 
 
 const HeroSection = () => {
@@ -8,10 +9,14 @@ const HeroSection = () => {
       <div className="banner">
         <img src={Image} width="100%" alt="hero_section" />
         <div className="heading_container">
-          <h1 className="rainbow_text">
-            Ipsita Priyadarsini
-          </h1>
-          <h2>Brings ideas to life with code</h2>
+          <Bounce>
+            <h1 className="rainbow_text">
+              Ipsita Priyadarsini
+            </h1>
+          </Bounce>
+          <Slide>
+            <h2>Brings ideas to life with code</h2>
+          </Slide>
           <h4> Software Engineer.FrontEnd Developer.ReactJs Developer </h4>
         </div>
       </div>
