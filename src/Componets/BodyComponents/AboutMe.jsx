76e36411fac6ee558f590df51aa0b9070c95d441
ipsitapriyadarsini <41966804+ipsitapriyadarsini@users.noreply.github.com>
2023-11-20 10:19:57 +0530

    Changes in about page

--- src/Componets/BodyComponents/AboutMe.jsx
+++ src/Componets/BodyComponents/AboutMe.jsx
@@ -4,27 +4,26 @@ import Pooja from "../../assets/images/rmbpooja.png";
 const AboutMe = () => {
   return (
 
-    <>
-      <div className="aboutme_container">
-        <div className="content">
-          <h2>ABOUT ME</h2>
-          <br />
-          <h3 style={{ textAlign: 'center' }}>A dedicated Front-end Developer based in Puri, Odisha, India</h3>
-          <br />
-          <p>
-            As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Typescript, Bootstrap, Tailwind,  SCSS and Material-UI.
-            I excel in designing and maintaining responsive
-            websites that offer a smooth user experience.</p>
-          <p>
+    <div className="aboutme_container" id="about">
+      <div className="content">
+        <h2>ABOUT ME</h2>
+        <br />
+        <h3 style={{ textAlign: 'center' }}>A dedicated Front-end Developer based in Puri, Odisha, India</h3>
+        <br />
+        <p>
+          As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Typescript, Bootstrap, Tailwind,  SCSS and Material-UI.
+          I excel in designing and maintaining responsive
+          websites that offer a smooth user experience.</p>
+        <p>
           My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques.
           I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
-          <br />
-        </div>
-        <div className="ipsita">
-          <img src={Pooja} alt="Ipsita" />
-        </div>
-      </div >
-    </>
+        <br />
+      </div>
+      <div className="ipsita">
+        <img src={Pooja} alt="Ipsita" />
+      </div>
+    </div >
+
   )
 }
 
--- src/Componets/headerComponents/Menu.jsx
+++ src/Componets/headerComponents/Menu.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { menuListItems } from '../ListComponent/MenuItems'
+import { Link } from 'react-router-dom'
 
 const Menu = () => {
     return (
         <section className="list-menu-items">
             <ul className="list_menu">
                 {menuListItems.map((item) => (
-                    <li className="list_items" id={item.id} key={item.id}>{item.title}</li>
+                    < li className="list_items" id={item.id} key={item.id} >
+                        <Link to={`#${item.id}`}>{item.title}</Link>
+                    </li>
                 ))}
             </ul>
-        </section>
+        </section >
     )
 }
 
