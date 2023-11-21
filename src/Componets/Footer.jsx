eaf901eec8ca95ba566130df4fcd4bd314a2830e
ipsitapriyadarsini <41966804+ipsitapriyadarsini@users.noreply.github.com>
2023-11-21 22:06:44 +0530

    changed in footer page

--- src/Componets/Footer.jsx
+++ src/Componets/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='parent-footer'>
-
+      hello
       <p className='copyright'>Copyright © 2022 | All Rights Reserved by Ipsita</p>
     </footer>
   )
